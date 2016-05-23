f08b4fcf65a5a7eb30d95cfa16e0ab001cff873f
Fredrik <fredrik.ekblad@gmail.com>
2016-05-23 23:17:48 +0200

    testing

---
+++ Search_Primes/Search_Primes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            int n = 1000;
+            int t;
+
+            for (int i = 1; i < n; i++)
+            {
+                t = i;
+                temp = 1;
+
+                for (int j = 1; j < i; j++)
+                {
+                    if (i%j != 0)
+                        temp++;
+
+                }
+
+                if (temp == i)
+                    Console.WriteLine(t);
+
+            }
+
+
+        }
+    }
+}
